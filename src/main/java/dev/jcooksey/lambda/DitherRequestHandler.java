80b538bf35a41513aa9c85b03c14da6b687b2632
Josiah Cooksey <jcsoftwaretests@gmail.com>
2025-10-27 21:32:01 -0500

    moves file for better organisation

--- src/main/java/dev/jcooksey/lambda/DitherRequestHandler.java
+++ src/main/java/dev/jcooksey/lambda/DitherRequestHandler.java
@@ -1,4 +1,4 @@
-package dev.jcooksey;
+package dev.jcooksey.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
@@ -36,5 +36,15 @@ public class DitherRequestHandler implements RequestHandler<APIGatewayProxyReque
         response.setBody("{\"error\": \"Input image was not Base64 encoded.\"}");
         response.setStatusCode(400);
         return response;
+
+        // TODO: use Apache Commons FileUpload library to parse the body of the request (multipart/form-data)
+        // it needs to be converted from Base64 into binary, then parsed into the constituent form fields
+        // afterwards, we need to validate that the expected data was uploaded
+        // if it's valid, we can perform the dithering
+        // finally, for this lambda module specifically, we need to package the data to return it
+        // I'd like to use json formatting because the website can then handle the response in a more standard way
+        // but the downside is that we need to encode it as base64, which uses more processing time both in this back-end as well as on the front-end
+        // instead, it may be possible to return a image/png response for valid requests but a json response for any issues
+        // that way, the front-end doesn't need to perform much extra work other than determining the response type and displaying the appropriate image or error message
     }
 }
