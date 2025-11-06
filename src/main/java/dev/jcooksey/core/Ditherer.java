a19623972d9769549ba02ef2108d26461ad4781d
Josiah Cooksey <jcsoftwaretests@gmail.com>
2025-11-06 17:08:50 -0600

    adjusts logic for better performance

--- src/main/java/dev/jcooksey/core/Ditherer.java
+++ src/main/java/dev/jcooksey/core/Ditherer.java
@@ -1,22 +1,36 @@
 package dev.jcooksey.core;
 
 import javax.imageio.ImageIO;
+import javax.xml.crypto.Data;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferInt;
+import java.awt.image.Raster;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Ditherer
 {
+    public static ArrayList<Color> palette;
+
+    public void setPalette(BufferedImage image)
+    {
+        palette = getImageColors(image);
+    }
+
+    public void setPalette(ArrayList<Color> colors)
+    {
+        palette = colors;
+    }
 
     public BufferedImage dither(Map<String, BufferedImage> images)
     {
-        ArrayList<Color> paletteColors = getImageColors(images.get("palette"));
+        setPalette(images.get("palette"));
 
-        BufferedImage ditheredImage = simpleDither(images.get("inputImage"), paletteColors);
+        BufferedImage ditheredImage = simpleDither(images.get("inputImage"));
 
         /*try
         {
@@ -32,19 +46,19 @@ public class Ditherer
     }
 
     // picks the closest color and propagates the error to the next pixel
-    public BufferedImage simpleDither(BufferedImage inputImage, ArrayList<Color> paletteColors)
+    public BufferedImage simpleDither(BufferedImage inputImage)
     {
         BufferedImage outputImage = new BufferedImage(inputImage.getWidth(), inputImage.getHeight(), inputImage.getType());
-
+        DataBufferInt dataBuffer = (DataBufferInt) inputImage.getRaster().getDataBuffer();
+        int[] pixels = dataBuffer.getData();
         BigColor totalErrors = new BigColor(0, 0, 0);
         for (int x = 0; x < inputImage.getWidth(); x++)
         {
             for (int y = 0; y < inputImage.getHeight(); y++)
             {
-                Color originalPixelColor = new Color(inputImage.getRGB(x, y));
-                BigColor targetColor = new BigColor(originalPixelColor);
+                BigColor targetColor = new BigColor(pixels[y * inputImage.getWidth() + x]);
                 targetColor.addError(totalErrors);
-                Color ditherColor = getNearestColor(paletteColors, targetColor);
+                Color ditherColor = getNearestColor(targetColor);
 
                 outputImage.setRGB(x, y, ditherColor.getRGB());
 
@@ -76,20 +90,19 @@ public class Ditherer
         return colors;
     }
 
-    private Color getNearestColor(ArrayList<Color> availableColors, BigColor targetColor)
+    private Color getNearestColor(BigColor targetColor)
     {
 
         double leastDistance = Double.MAX_VALUE;
         Color leastColor = null;
-        for (Color color : availableColors)
+        for (Color color : palette)
         {
             double newDistance = targetColor.getColorDistance(color);
-            if (newDistance >= leastDistance)
+            if (newDistance < leastDistance)
             {
-                continue;
+                leastDistance = newDistance;
+                leastColor = color;
             }
-            leastDistance = newDistance;
-            leastColor = color;
         }
         return leastColor;
     }
