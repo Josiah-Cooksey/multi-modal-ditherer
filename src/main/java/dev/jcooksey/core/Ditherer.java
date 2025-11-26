c8c0381589706f60d748b061c7d2d5526e9df70e
Josiah Cooksey <jcsoftwaretests@gmail.com>
2025-11-25 22:20:30 -0600

    clamps errors for linear dithering after each step

--- src/main/java/dev/jcooksey/core/Ditherer.java
+++ src/main/java/dev/jcooksey/core/Ditherer.java
@@ -52,6 +52,8 @@ public class Ditherer
 
                 totalErrors = targetColor;
                 totalErrors.removeColor(ditherColor);
+
+                totalErrors.clampColor();
             }
         }
 
